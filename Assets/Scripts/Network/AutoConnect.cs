d2a9f2cc2fcee02e57a88f1d407cef911b82dddd
saarg <saarg>
2018-01-27 20:02:07 -0500

    Added Join and create game

---
+++ Assets/Scripts/Network/AutoConnect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NetworkManager))]
+
+public class AutoConnect : MonoBehaviour {
+
+	public bool startServer = false;
+
+	void Start () {
+		#if UNITY_EDITOR
+			GetComponent<NetworkManagerHUD>().enabled = true;
+		#else
+			GetComponent<NetworkManagerHUD>().enabled = false;			
+		#endif
+
+		if (HeadlessModeDetection.IsHeadlessMode() || startServer) {
+			GetComponent<NetworkManager>().StartServer();
+		}
+		else if (PlayerPrefs.GetString("ip") != null && PlayerPrefs.GetString("ip") != "") {
+			NetworkManager nm = GetComponent<NetworkManager>();
+
+			nm.networkAddress = PlayerPrefs.GetString("ip");
+			nm.StartClient();
+		}
+	}
+
+}
---
+++ Assets/Scripts/UI/UIMenuMethods.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 	Class used in menus tu give simple acces in unityevents to some methods
+/// </summary>
+public class UIMenuMethods : MonoBehaviour {
+
+	/// <summary>
+	/// 	Load async scene, default single mode
+	/// </summary>
+	/// <param name="name">scene name or path</param>  
+	public void LoadScene(string name) {
+		SceneManager.LoadSceneAsync(name, LoadSceneMode.Single);
+	}
+
+	/// <summary>
+	/// 	Set ip in player pref
+	/// </summary>
+	/// <param name="ip">ip for remote server</param>  
+	public void SetServerIp(string ip) {
+		PlayerPrefs.SetString("ip", ip);
+	}
+
+	/// <summary>
+	/// 	Application quit
+	/// </summary>
+	public void Quit() {
+		Application.Quit();
+	}
+}
