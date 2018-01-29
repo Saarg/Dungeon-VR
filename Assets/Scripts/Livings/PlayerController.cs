a05d933a9b703e210008edba01f58f56775f5559
Erwann <erwann.large@laposte.net>
2018-01-29 16:13:13 -0500

    Add player movements + rotation with camera.

--- Assets/Scripts/Livings/PlayerController.cs
+++ Assets/Scripts/Livings/PlayerController.cs
@@ -10,12 +10,16 @@ using UnityEngine.Networking;
 /// </summary>
 public class PlayerController : Living {
 
-	public Camera cam;
+	//public Camera cam;
+	Transform cam;
+
+	private float turnSpeed = 50;
 
 	public Transform spellOrigin;
 	public GameObject ammo;
 
 	private Animator _animator;
+	private Rigidbody rigidBody;
 
 	/// <summary>  
 	/// 	Fetch animator
@@ -23,6 +27,8 @@ public class PlayerController : Living {
 	/// </summary>
 	void Start() {
 		_animator = GetComponent<Animator>();
+		rigidBody = GetComponent<Rigidbody>();
+		cam = Camera.main.transform;
 
 		if (!isLocalPlayer) {
 			Destroy(cam.gameObject);
@@ -35,7 +41,7 @@ public class PlayerController : Living {
 	/// </summary>
 	void Update () {
 		if (isLocalPlayer) {
-			float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+			/*float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 			float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
 			transform.Rotate(0, x, 0);
@@ -49,8 +55,35 @@ public class PlayerController : Living {
 				_animator.SetTrigger("Jump");
 			}
 
-			_animator.SetFloat("Speed", z);
-		}
+			_animator.SetFloat("Speed", z);*/
+
+		    ///////////////////////////////////////////////////////////////////////////////////////////////////////
+			Vector3 dir = (cam.right * Input.GetAxis("Horizontal") * Time.deltaTime) + (cam.forward * Input.GetAxis("Vertical") * Time.deltaTime);
+            dir.y = 0;
+
+			if (Input.GetAxis("Horizontal") != 0 ||Input.GetAxis("Vertical") != 0)
+			{      
+				rigidBody.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (dir), turnSpeed * Time.deltaTime);
+				rigidBody.velocity = transform.forward * speed;
+                //_animator.SetTrigger("Running");;//Animation de course
+                _animator.SetFloat("Speed", Input.GetAxis("Vertical") * Time.deltaTime * speed);
+            }
+            else
+			{
+                //_animator.SetTrigger("Idle");;//animation Idle
+            }
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////
+            if (Input.GetButtonDown("Fire1")) {
+				CmdFire();
+			}
+
+			if (Input.GetButtonDown("Jump") && canJump) {
+				_animator.SetTrigger("Jump");
+			}
+
+            
+
+        }
 	}
 
 	/// <summary>  
