3710c5ff9a8c8eb9ef071396f2b3b2bf1ef64966
y3lousso <y3lousso@gmail.com>
2018-01-30 18:16:36 -0500

    Add VR deplacement + pick/drop trap from table to dungeon

---
+++ Assets/Scripts/VR/TrapDropZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDropZone : MonoBehaviour {
+
+    public Trap placedTrap;
+
+    public void AttachTrapToDropZone(object o, VRTK.InteractableObjectEventArgs e)
+    {
+        
+        if (placedTrap == null)
+        {
+            // Need to add a way to rotate the trap (Touchpad should be find for that)
+            placedTrap = Instantiate(e.interactingObject.GetComponent<TrapControllerManager>().selectedTrap, transform.position, transform.rotation);
+            placedTrap.transform.parent = transform;
+        }
+        else
+        {
+            // error feedback for impossible drop
+        }
+        
+    }
+
+    public void RemoveTrapFromDropZone(object o, VRTK.InteractableObjectEventArgs e)
+    {
+        Destroy(placedTrap.gameObject);
+    }
+}
