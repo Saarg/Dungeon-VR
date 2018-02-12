54c63f05650e523d0be2b338ac94eabbbdda42d8
Winnilis <agathe.guillemot@gmail.com>
2018-02-12 00:31:06 -0500

    start creation of explosion spell ; firering is working

---
+++ Assets/Scripts/Spells/ClassSpells/ExplosionSpell.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionSpell : Spell {
+
+	[SerializeField] private GameObject explosionPrefab;
+	[SerializeField] private Transform spawningPoint;
+
+	protected override void Effects(){
+//		Instantiate(wallPrefab, spawningPoint.transform.position, spawningPoint.transform.rotation);
+
+		spawningPoint = caster.transform;
+		Instantiate(explosionPrefab, spawningPoint.position + spawningPoint.transform.forward*2, spawningPoint.rotation);
+	}
+}
---
+++ Assets/Scripts/Spells/ClassSpells/MagicalExplosion.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicalExplosion : MonoBehaviour {
+
+	[SerializeField] private ParticleSystem firering;
+	[SerializeField] private ParticleSystem.ShapeModule fireringShape;
+	[SerializeField] private SphereCollider aoe;
+	[SerializeField] private int damage;
+	[SerializeField] private bool isBurning = false;
+
+	private float fireringMaxRadius = 7f;
+	private float aoeMaxRadius = 1.8f;
+
+//	[SerializeField] private float spawningTime;
+	[SerializeField] private float burningTime;
+
+	void Start(){
+		fireringShape = firering.GetComponent<ParticleSystem> ().shape;
+
+		fireringShape.radius = 0;
+		aoe.radius = 0;
+	}
+
+	void Update(){
+		if (fireringShape.radius < fireringMaxRadius) GrowRadius ();
+		else StartCoroutine ("Burning");
+	}
+
+	private void GrowRadius(){
+		fireringShape.radius += fireringMaxRadius * Time.deltaTime;
+		aoe.radius += aoeMaxRadius * Time.deltaTime;
+	}
+
+	IEnumerator Burning(){
+		isBurning = true;
+		float t = 0;
+		while (t < burningTime) {
+			t += Time.deltaTime;
+			yield return 0;
+		}
+		DestroyExplosion ();
+	}
+
+	void OnTriggerEnter(Collider _col){
+		if (_col.gameObject.CompareTag ("Enemy") && isBurning) {
+			_col.gameObject.GetComponent<Living> ().TakeDamage(damage, Bullet.DamageTypeEnum.fire);
+		}
+	}
+
+	private void DestroyExplosion(){
+		GameObject.Destroy (this.gameObject);
+	}
+}
