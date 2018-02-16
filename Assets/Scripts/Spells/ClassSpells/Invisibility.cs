124a1b84f0a9f22e8e47034a79ad2205fb38f153
saarg <milsonneau.jean@free.fr>
2018-02-16 12:54:49 -0500

    improved bad practive in invisibility

--- Assets/Scripts/Spells/ClassSpells/Invisibility.cs
+++ Assets/Scripts/Spells/ClassSpells/Invisibility.cs
@@ -4,72 +4,63 @@ using UnityEngine;
 
 public class Invisibility : Spell
 {
-
-    //[SerializeField] Texture invisibilityTexture = null;
     [SerializeField] Color colstart;
     [SerializeField] Color colend;
     [SerializeField] float Duration;
 
+    Renderer[] _renderers;
+
     protected override void Start()
     {
         lastActivation = cooldown;
-        //basedTexture = caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().sharedMaterial;
-        colstart = new Color(caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color.r,
-            caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color.g,
-            caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color.b,
-            0.1f);
-        colend = caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color;
+
+        _renderers = caster.GetComponentsInChildren<Renderer>();
+
+        if (_renderers.Length > 0) {
+            colstart = _renderers[0].material.color;
+            colstart.a = 0.1f;
+            colend = _renderers[0].material.color;
+        }
     }
 
     protected override void Effects()
     {
-        //Stupid bullshit engine
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.SetFloat("_Mode", 3.0f);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.SetInt("_ZWrite", 0);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.DisableKeyword("_ALPHATEST_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.EnableKeyword("_ALPHABLEND_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.renderQueue = 3000;
+        foreach (Renderer r in _renderers) {
+            foreach (Material mat in r.materials) {
+                mat.SetFloat("_Mode", 3.0f);
+                mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                mat.SetInt("_ZWrite", 0);
+                mat.DisableKeyword("_ALPHATEST_ON");
+                mat.EnableKeyword("_ALPHABLEND_ON");
+                mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                mat.renderQueue = 3000;
+                
+                mat.color = colstart;
+                mat.color = colstart;
+            }
+        }
 
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.SetFloat("_Mode", 3.0f);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.SetInt("_ZWrite", 0);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.DisableKeyword("_ALPHATEST_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.EnableKeyword("_ALPHABLEND_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.renderQueue = 3000;
-
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color = colstart;
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.color = colstart;
         StartCoroutine("InvisibilityPeriode");
     }
 
     protected void EndEffects()
     {
-        //stupid bullshi engin 2
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.SetFloat("_Mode", 0.0f);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.SetInt("_ZWrite", 1);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.DisableKeyword("_ALPHATEST_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.EnableKeyword("_ALPHABLEND_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.renderQueue = -1;
+        foreach (Renderer r in _renderers) {
+            foreach (Material mat in r.materials) {
+                mat.SetFloat("_Mode", 0.0f);
+                mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                mat.SetInt("_ZWrite", 1);
+                mat.DisableKeyword("_ALPHATEST_ON");
+                mat.EnableKeyword("_ALPHABLEND_ON");
+                mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                mat.renderQueue = -1;
 
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.SetFloat("_Mode", 0.0f);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.SetInt("_ZWrite", 1);
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.DisableKeyword("_ALPHATEST_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.EnableKeyword("_ALPHABLEND_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.renderQueue = -1;
-
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material.color = colend;
-        caster.gameObject.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().material.color = colend;
+                mat.color = colend;
+                mat.color = colend;
+            }
+        }
     }
 
     IEnumerator InvisibilityPeriode()
