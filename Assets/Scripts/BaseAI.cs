ba51497feeba2572db717d61b7c39e2291762b06
Daniel Jomphe <danieljomphework@gmail.com>
2018-04-11 14:35:57 -0400

    set weapon drop change to 40%

--- Assets/Scripts/BaseAI.cs
+++ Assets/Scripts/BaseAI.cs
@@ -567,7 +567,7 @@ public class BaseAI : NetworkBehaviour {
         }
         CmdSetBool("moving", false);
         gameObject.GetComponent<Living>().OnDeath -= OnDeath;
-        if (isServer && Random.Range(0f, 1f) < 0.4f)
+        if (isServer)
         {
             RaycastHit hit;
             Vector3 pos = transform.position;
